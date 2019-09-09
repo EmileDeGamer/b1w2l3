14f182e00d74e0c22542cb7627ecdb70ee9f260e
EmileDeGamer <emilemolzoho@gmail.com>
2019-09-09 14:50:21 +0200

    init

---
+++ b1w2l3.js
@@ -0,0 +1,29 @@
+let ui = prompt('Vul getal in', 'Getal')
+test(12, ui)
+function test(getal, getal2) {
+    document.write(getal + '\n')
+    let tempGetal = getal
+    getal = getal += 6
+    document.write(tempGetal + ' + 6 = ' + getal + '\n')
+    tempGetal = getal
+    getal = getal * getal2
+    document.write(tempGetal + ' * ' + getal2 + ' = ' + getal + '\n')
+    tempGetal = getal
+    getal = getal / 5
+    document.write(tempGetal + ' / 5 = ' + getal + '\n')
+    tempGetal = getal
+    getal = getal -= 12
+    document.write(tempGetal + ' - 12 = ' + getal + '\n')
+}
+
+let tafel = prompt('Welke tafel?', 'Tafel')
+let hoeveelheid = prompt('Hoeveel keer?', 'Hoeveelheid')
+tafels(tafel, hoeveelheid)
+function tafels(tafel, hoeveelheid) {
+    for (let hoeveelheidT = 0; hoeveelheidT <= hoeveelheid; hoeveelheidT++) {
+        //const element = array[hoeveelheidT];
+        
+        let antwoord = tafel * hoeveelheidT
+        document.write(tafel + ' * ' + hoeveelheidT + ' = ' + antwoord + '\n')
+    }
+}
